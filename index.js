fb47fcc6419dec8473e30a25ef4ca52e3c80c88d
dylankgirard <dylankgirard@gmail.com>
2020-07-05 14:20:31 -0700

    Added some notes to clarify JS logic so far

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const player = {
-	playerChoice: [],
-};
+// The player will input their choices here
+let playerChoice = [];
 
-const computer = {
-	computerChoice: [],
-};
-
+// A randomizer for the computer to choose a value from 0 - 4
 const randomChoice = () => Math.floor(Math.random() * 5);
 
-let round0 = 3;
+// Round0 has a value of three since the computer's intital number of choices will be 3. This value will increase by 1 each round.
+let round = 3;
 
+// A function to push generated values to the compChoice array by round number of times.
 const computerArray = () => {
-	let c = [];
-	for (let i = 0; i < round0; i++) {
-		c.push(randomChoice());
+	let compChoice = [];
+	for (let i = 0; i < round; i++) {
+		compChoice.push(randomChoice());
 	}
-	return c;
+	return compChoice;
 };
 
-const advanceRound = () => round0++;
+// A function to advance the round by 1
+const advanceRound = () => round++;
 
+// TESTING
+advanceRound();
 advanceRound();
 advanceRound();
 
