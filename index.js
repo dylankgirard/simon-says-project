764646653186cb2d2af3605e140bdfa151512053
dylankgirard <dylankgirard@gmail.com>
2020-07-10 11:29:06 -0700

    Added pointer hover to main buttons and a JS function that will compare human/computer choice arrays

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-// The player will input their choices here
+// The player and computer will input their choices here
+
 let playerChoice = [];
+let compChoice = [];
+
+const playerChoiceToString = () => playerChoice.toString();
+const compChoiceToString = () => compChoice.toString();
 
 // A randomizer for the computer to choose a value from 0 - 4
+
 const randomChoice = () => Math.floor(Math.random() * 5);
 
 // Round0 has a value of three since the computer's intital number of choices will be 3. This value will increase by 1 each round.
+
 let round = 3;
 
 // A function to push generated values to the compChoice array by round number of times.
-const computerArray = () => {
-	let compChoice = [];
+
+const computerChoiceGenerator = () => {
 	for (let i = 0; i < round; i++) {
 		compChoice.push(randomChoice());
 	}
@@ -17,9 +24,28 @@ const computerArray = () => {
 };
 
 // A function to advance the round by 1
-const advanceRound = () => round++;
+
+const advanceRound = () => {
+	compChoice = [];
+	round++;
+};
+
+// A function that will compare computer-choices to human-choices that equates to TRUE if they are equal and FALSE if they are not.
+
+const compareChoiceArrays = () => {
+	if (playerChoiceToString() === compChoiceToString()) {
+		return true;
+	} else {
+		return false;
+	}
+};
 
 // TESTING
-advanceRound();
 
-console.log(computerArray());
+
+// Arrays that are converted to strings can be compared with strict equality
+// let array1 = [1, 2, 3]
+// let array2 = [1, 2, 3] <-- These two values equate to FALSE when compared
+
+// let arrayToString1 = array1.toString()
+// let arrayToString2 = array2.toString() <-- These two values equate to TRUE when compared
